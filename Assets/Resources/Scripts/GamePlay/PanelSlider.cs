77664e74bfa24fb29733ca98df9f74877174a9d4
Bubble <2425569471@qq.com>
2023-03-12 16:46:13 +0800

    RulePanel UI Polish

--- Assets/Resources/Scripts/GamePlay/PanelSlider.cs
+++ Assets/Resources/Scripts/GamePlay/PanelSlider.cs
@@ -10,15 +10,29 @@ public class PanelSlider : MonoBehaviour
     SmoothObject motion;
     List<Transform> pages;
     public KeyCode leftKey, rightKey;
-    Vector3 originalSize;
+    public GameObject smoothCirclePrefab;
+    ItemLayout indexLayout;
+    List<SmoothObject> indexes;
+    GameObject slider;
+    public Vector3 indexSize0, indexSize1;
+    public Color indexColor0, indexColor1;
+    SmoothObject lastEnlightedIndex;
+    SmoothObject leftArrow, rightArrow;
     void Awake()
     {
-        motion = gameObject.AddComponent<SmoothObject>();
+        slider = transform.Find("Slider").gameObject;
+        leftArrow = transform.Find("leftArrow").GetComponent<SmoothObject>();
+        rightArrow = transform.Find("rightArrow").GetComponent<SmoothObject>();
+
+        motion = slider.AddComponent<SmoothObject>();
         motion.posFollowSpeed = 30f;
         motion.sizeFollowSpeed = 40f;
 
         pages = new List<Transform>();
-        originalSize = transform.localScale;
+        indexLayout = transform.Find("IndexLayout").GetComponent<ItemLayout>();
+        indexes = new List<SmoothObject>();
+        lastEnlightedIndex = null;
+        curPage = 0;
     }
     void Start()
     {
@@ -29,6 +43,18 @@ public class PanelSlider : MonoBehaviour
     void Update()
     {
         UserInput();
+        if (pageCount < 1) {
+            leftArrow.targetSize = new Vector2(0, 0.3f);
+            rightArrow.targetSize = new Vector2(0, 0.3f);
+        }
+        else
+        {
+            if (curPage > 0) leftArrow.targetSize = new Vector2(1, 1);
+            else leftArrow.targetSize = new Vector2(0, 0.3f);
+
+            if (curPage < pageCount - 1) rightArrow.targetSize = new Vector2(1, 1);
+            else rightArrow.targetSize = new Vector2(0, 0.3f);
+        }
     }
     void UserInput()
     {
@@ -50,10 +76,29 @@ public class PanelSlider : MonoBehaviour
     }
     public void AppendPage(Transform newPage)
     {
-        newPage.transform.SetParent(transform);
+        newPage.transform.SetParent(slider.transform);
         pages.Add(newPage);
         pageCount++;
+        GameObject newCircle = Instantiate(smoothCirclePrefab);
+        indexes.Add(newCircle.GetComponent<SmoothObject>());
+        indexLayout.AppendItem(newCircle.transform);
+        newCircle.GetComponent<SmoothObject>().targetColor = indexColor0;
+        newCircle.GetComponent<SmoothObject>().targetSize = indexSize0;
+        if (pageCount == 1) EnlightNewIndex();
         UpdatePos();
+    }
+    public void EnlightNewIndex()
+    {
+        Debug.Log("curPage: " + curPage + "  indexCount:" + indexes.Count);
+        if (lastEnlightedIndex == indexes[curPage]) return;
+        if (lastEnlightedIndex != null && indexes[curPage] != lastEnlightedIndex)
+        {
+            lastEnlightedIndex.targetSize = indexSize0;
+            lastEnlightedIndex.targetColor = indexColor0;
+        }
+        lastEnlightedIndex = indexes[curPage];
+        indexes[curPage].targetSize = indexSize1;
+        indexes[curPage].targetColor = indexColor1;
     }
     public void Clear()
     {
@@ -63,6 +108,7 @@ public class PanelSlider : MonoBehaviour
     }
     void UpdatePos()
     {
+        EnlightNewIndex();
         float pos = 0;
         for (int i = 0; i < pages.Count; i++)
         {
--- Assets/Resources/Scripts/Utils/SmoothObject.cs
+++ Assets/Resources/Scripts/Utils/SmoothObject.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class SmoothObject : MonoBehaviour
 {
     public Vector2 targetPos;
+    public Color targetColor = new Color(1,1,1,1);
     public float posFollowSpeed = 1f;
     public Vector2 targetSize;
     public float sizeFollowSpeed = 1f;
+    public float colorFollowSpeed = 30f;
     public bool settledDown;
+    public SpriteRenderer colorSprite;
     virtual protected void Awake()
     {
-        targetSize = transform.localScale;
+        if (targetSize.magnitude < 0.001f) targetSize = transform.localScale;
         settledDown = false;
     }
     virtual protected void Start()
@@ -25,6 +28,7 @@ public class SmoothObject : MonoBehaviour
         //targetPos = Global.mainCam.ScreenToWorldPoint(Input.mousePosition);
         CheckPos();
         CheckSize();
+        CheckColor();
     }
     virtual public void SelfDestroy()
     {
@@ -47,5 +51,12 @@ public class SmoothObject : MonoBehaviour
         Vector3 offset = (Vector3)targetSize - transform.localScale;
         if (offset.magnitude < 0.01f) transform.localScale = targetSize;
         else transform.localScale = Vector3.Lerp(transform.localScale, targetSize, sizeFollowSpeed * Time.deltaTime);    
+    }
+    void CheckColor()
+    {
+        if (colorSprite == null) return;
+        Color offset = targetColor - colorSprite.color;
+        if (((Vector4)offset).magnitude < 0.01f) colorSprite.color = targetColor;
+        else colorSprite.color = Color.Lerp(colorSprite.color, targetColor, colorFollowSpeed * Time.deltaTime);
     }
 }
